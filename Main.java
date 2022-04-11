111d50f88eed8df758ba609040186a89fa3da271
cibo-sairai <99768829+cibo-sairai@users.noreply.github.com>
2022-04-11 13:53:07 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,42 @@
+package com.zaremba.graphs;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static ArrayList<Node> graph;
+    //private  int vertex; /* total number number of vertices in the graph */    
+    private LinkedList<Integer> adj[];      /* adjacency list */    
+    private Queue<Integer> queue;           /* maintaining a queue */  
+    
+    public static void main(String[] args) throws FileNotFoundException {
+        setupTree();
+        BFS();
+    }
+
+    private static void BFS() {
+        
+// put bfs code here
+   
+    }
+
+    private static void setupTree() throws FileNotFoundException {
+        File f = new File("tree.txt");
+        graph = new ArrayList<>();
+        Scanner scan = new Scanner(f);
+        while(scan.hasNext()){
+            String line = scan.nextLine();
+            parseLine(line);
+        }
+    }
+
+    private static void parseLine(String line) {
+        String[] keys = line.split(" ");
+        int key = Integer.parseInt(keys[0]);
+        ArrayList<Integer> points = new ArrayList<>();
+        for(int i = 1; i < keys.length;i++){
+            points.add(Integer.parseInt(keys[i]));
+        }
+        graph.add(new Node(key, points));
+    }
+}
